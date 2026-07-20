7cc550921a09a1c3349cb681244269a7a33d078a
ttmassa <timothee.massa.perso@gmail.com>
2026-07-21 01:23:45 +0200

    feat: setup wxt and cloudfare worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -13,14 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
+		return new Response("Hello World!");
 	},
 } satisfies ExportedHandler<Env>;
