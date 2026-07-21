2493787e6c0441d8110ea4930b966dfeb2ab5ea2
ttmassa <timothee.massa.perso@gmail.com>
2026-07-21 16:55:23 +0200

    feat: listen for message from content script

--- wxt/entrypoints/background.ts
+++ wxt/entrypoints/background.ts
@@ -1,3 +1,11 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log('[Background] Service Worker started.');
+
+  // Listen for messages from content scripts
+  browser.runtime.onMessage.addListener((message, sender) => {
+    if (message.type === 'CHECK_TEXT') {
+      console.log('[Background] Received text from content script:', message.payload);
+    }
+  });
+  
 });
