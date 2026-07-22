3a1dc036100af25a185498b3035edbb2f715a142
ttmassa <timothee.massa.perso@gmail.com>
2026-07-23 01:18:06 +0200

    fix: remove asset import in popup panel

--- wxt/entrypoints/popup/App.tsx
+++ wxt/entrypoints/popup/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import './App.css';
 
@@ -11,9 +10,6 @@ function App() {
       <div>
         <a href="https://wxt.dev" target="_blank">
           <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>WXT + React</h1>
